8e3df053e4451a986cce9e87d61ef0945795701a
MShoaei <mohammad.shoaei@outlook.com>
2019-02-21 16:37:33 +0330

    turn into API.

--- actions/home.go
+++ actions/home.go
@@ -1,13 +1,9 @@
 package actions
 
-import (
-	"github.com/MShoaei/command_control/models"
-	"github.com/gobuffalo/buffalo"
-)
+import "github.com/gobuffalo/buffalo"
 
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
-	c.Set("user", models.User{})
-	return c.Render(200, r.HTML("index.html"))
+	return c.Render(200, r.JSON(map[string]string{"message": "Welcome to Buffalo!"}))
 }
