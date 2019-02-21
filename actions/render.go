0fb2c83395b9d867610ec4c6bbac2963f64e80e7
MShoaei <mohammad.shoaei@outlook.com>
2019-02-21 16:45:37 +0330

    profiles folder path

--- actions/render.go
+++ actions/render.go
@@ -1,10 +1,13 @@
 package actions
 
 import (
+	"path/filepath"
+
 	"github.com/gobuffalo/buffalo/render"
 )
 
 var r *render.Engine
+var Profiles = filepath.Join(".", "profiles")
 
 func init() {
 	r = render.New(render.Options{
