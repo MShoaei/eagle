923a809d0528ab583b2804dc1932da8b6e0913f8
MShoaei <mohammad.shoaei@outlook.com>
2019-02-23 21:31:12 +0330

    Bot models and Database migrations

---
+++ migrations/20190222225539_bots.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE bots(
+    "id" character varying(255) PRIMARY KEY NOT NULL,
+    "ip" character varying(255) NOT NULL,
+    "whoami" character varying(255) NOT NULL,
+    "os" character varying(255) NOT NULL,
+    "install_date" character varying(255) NOT NULL,
+    "admin" boolean NOT NULL,
+    "av" character varying(255) NOT NULL,
+    "cpu" character varying(255) NOT NULL,
+    "gpu" character varying(255) NOT NULL,
+    "version" character varying(255) NOT NULL, 
+    "last_checkin" character varying(255),
+    "last_command" character varying(255),
+    "new_command" character varying(255) 
+);
--- models/Bot.go
+++ models/Bot.go
@@ -1,17 +1,17 @@
 package models
 
 type Bot struct {
-	ID          string
-	IP          string
-	WhoAmI      string
-	Os          string
-	InstallDate string
-	Admin       bool
-	Av          string
-	CPU         string
-	Gpu         string
-	Version     string
-	LastCheckin string
-	LastCommand string
-	NewCommand  string
+	ID          string `db:"id"`
+	IP          string `db:"ip"`
+	WhoAmI      string `db:"whoami"`
+	Os          string `db:"os"`
+	InstallDate string `db:"install_date"`
+	Admin       bool   `db:"admin"`
+	Av          string `db:"av"`
+	CPU         string `db:"cpu"`
+	Gpu         string `db:"gpu"`
+	Version     string `db:"version"`
+	LastCheckin string `db:"last_checkin"`
+	LastCommand string `db:"last_command"`
+	NewCommand  string `db:"new_command"`
 }
